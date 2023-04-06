ca36fc61a14111056fdb3a13c7eb070f67b61ccf
Takenari-Yamamoto <tktkymnr1216@gmail.com>
2023-04-06 21:46:38 +0900

    add sql file

---
+++ backend/db/migrations/000001_tasks.up.sql
@@ -0,0 +1,8 @@
+CREATE TABLE tasks (
+  id SERIAL PRIMARY KEY,
+  title TEXT NOT NULL,
+  description TEXT,
+  completed BOOLEAN NOT NULL DEFAULT false,
+  created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+  updated_at TIMESTAMP NOT NULL DEFAULT NOW()
+);
