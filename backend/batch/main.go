c3d0402a81458adf9b465a4985071468bc88f042
Takenari-Yamamoto <tktkymnr1216@gmail.com>
2023-04-09 21:29:44 +0900

    fix

--- backend/batch/main.go
+++ backend/batch/main.go
@@ -5,33 +5,20 @@ import (
 	"fmt"
 
 	"github.com/Takenari-Yamamoto/golang-nextjs-app/db"
-	"github.com/Takenari-Yamamoto/golang-nextjs-app/db/models"
-	"github.com/volatiletech/null/v8"
-	"github.com/volatiletech/sqlboiler/v4/boil"
+	"github.com/Takenari-Yamamoto/golang-nextjs-app/repository"
 )
 
-func insertTask(count int) {
+func main() {
 	db, err := db.NewDb()
 	if err != nil {
-		fmt.Println("DB エラー", err)
-		return
-	}
-	defer db.Close()
-	task := &models.Task{
-		Title:       fmt.Sprintf("Dummy New Task %d", count),
-		Description: null.StringFrom("This is a new task"),
-	}
-	err = task.Insert(context.Background(), db, boil.Infer())
-	if err != nil {
-		fmt.Println("挿入失敗", err)
 		return
 	}
 
-	fmt.Println("User inserted successfully.")
-}
-
-func main() {
+	repo := repository.NewTodoRepo(db)
+	ctx := context.Background()
 	for i := 0; i < 100; i++ {
-		insertTask(i)
+		if err := repo.Create(ctx, fmt.Sprintf("Dummy New Task %d", i), "This is a new task"); err != nil {
+			fmt.Println("err YADE")
+		}
 	}
 }
