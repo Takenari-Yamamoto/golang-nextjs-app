2c2b08c27d8fcef63b94398784e524cfe917c72e
Takenari-Yamamoto <tktkymnr1216@gmail.com>
2023-05-03 17:05:37 +0900

    controller

--- backend/controller/task.go
+++ backend/controller/task.go
@@ -3,20 +3,36 @@ package controller
 import (
 	restApiModel "golang-nextjs-app/restapi/models"
 	"golang-nextjs-app/restapi/operations"
+	"golang-nextjs-app/usecase"
 
 	"github.com/go-openapi/runtime/middleware"
 	"github.com/go-openapi/strfmt"
 )
 
-type TaskController struct{}
+type TaskController struct {
+	taskUsecase *usecase.TaskUsecase
+}
 
 func NewTaskController() *TaskController {
 	return &TaskController{}
 }
 
-// func (c *TaskController) CreateTask(params operations.CreateTaskParams) middleware.Responder {
+func (c *TaskController) CreateTask(params operations.PostTaskParams) middleware.Responder {
 
-// }
+	var (
+		ok = operations.NewPostTaskCreated()
+	)
+
+	ctx := params.HTTPRequest.Context()
+	token := params.HTTPRequest.Header.Get("Authorization")
+
+	if _, err := c.taskUsecase.CreateTask(ctx, params.Title, params.Content, token); err != nil {
+		return operations.NewPostTaskInternalServerError()
+	}
+
+	return ok
+
+}
 
 func (c *TaskController) GetAllTasks(params operations.GetTasksParams) middleware.Responder {
 	var (
