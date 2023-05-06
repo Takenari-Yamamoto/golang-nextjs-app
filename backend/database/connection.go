c72ed8802e94abed178d8b9f26fc3b4ccc7893d9
Takenari-Yamamoto <tktkymnr1216@gmail.com>
2023-05-06 19:53:11 +0900

    フロントdocker諦めた

--- backend/database/connection.go
+++ backend/database/connection.go
@@ -10,16 +10,15 @@ import (
 type DSNParam string
 
 const (
-	Host     DSNParam = "127.0.0.1"
+	Host     DSNParam = "postgresql"
 	User     DSNParam = "postgres"
 	Password DSNParam = "password"
-	DBName   DSNParam = "golang-nextjs-db"
+	DBName   DSNParam = "golang_nextjs"
 	Port     DSNParam = "5432"
 )
 
 func NewDb() (*sql.DB, error) {
 	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=Asia/Tokyo",
-		"127.0.0.1", User, Password, DBName, Port)
-
+		Host, User, Password, DBName, Port)
 	return sql.Open("postgres", dsn)
 }
