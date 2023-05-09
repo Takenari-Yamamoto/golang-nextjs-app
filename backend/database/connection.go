9f1f15757676dbdffe24ac72fb115e54819f2f12
Takenari-Yamamoto <tktkymnr1216@gmail.com>
2023-05-09 21:31:08 +0900

    finally build

--- backend/database/connection.go
+++ backend/database/connection.go
@@ -3,6 +3,7 @@ package database
 import (
 	"database/sql"
 	"fmt"
+	"log"
 	"os"
 
 	_ "github.com/lib/pq"
@@ -24,6 +25,14 @@ func NewDb() (*sql.DB, error) {
 	password := os.Getenv(EnvPassword)
 	dbname := os.Getenv(EnvDBName)
 	port := os.Getenv(EnvPort)
+
+	log.Println(
+		"ホスト名---->>>>", host,
+		"ユーザー名---->>>>", user,
+		"パスワード---->>>>", password,
+		"DB名---->>>>", dbname,
+		"ポート番号---->>>>", port,
+	)
 
 	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=Asia/Tokyo",
 		host, user, password, dbname, port)
